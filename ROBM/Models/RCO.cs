d089ce7962ade6942ab8cf9790408457fe34478d
Dell Pc <mudasir9757@gmail.com>
2023-08-21 13:32:04 +0500

    Add rco model props

--- ROBM/Models/RCO.cs
+++ ROBM/Models/RCO.cs
@@ -10,8 +10,14 @@ namespace ROBM.Models
         public int RCO_Id { get; set; }
         public string Ref_No { get; set; }
         public int ERO_No { get; set; }
-
         public DateTime ERO_Issue_Date { get; set; }
-        public int Age { get; set0; }
+        public int ERO_Amount { get; set; }
+        public DateTime ERO_Implement_Date { get; set; }
+        public int Arrear { get; set; }
+        public int RCO_Fee { get; set; }
+        public string CRC_Remarks { get; set; }
+        public string User_Name { get; set; }
+        public DateTime Create_Date { get; set; }
+        public DateTime Create_Time { get; set; }
     }
 }
